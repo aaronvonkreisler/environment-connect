312c142be4f5977b8729a9ddf08697f5c7c57cb4
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-01-29 10:24:03 -0600

    theming

---
+++ src/constants/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+   colors: {
+      primary: '#1D1D1D',
+      textPrimary: '#E5E5E5',
+      textSecindary: '#848484',
+      background: '#212121',
+   },
+   device: {
+      mobile: '(max-width: 500px)',
+      mobileLarge: '(min-width: 501px) and (max-width: 767px)',
+      tablet: '(min-width: 768px) and (max-width: 1023px)',
+      laptop: '(min-width: 1024px) and (max-width: 2559px)',
+      desktop: '(min-width: 2560px)',
+   },
+};
+
+/* Responsive Variables */
+/* $break1:    "(max-width:499px)";
+$break2:    "(min-width:500px) and (max-width:1004px)";
+$break2open:"(min-width:500px)";
+$break3:    "(min-width:1005px) and (max-width:1094px)";
+$break3open:"(min-width:1005px)";
+$break4:    "(min-width:1095px) and (max-width:1281px)";
+$break4open:"(min-width:1095px)";
+$break5open:"(min-width:1282px)"; */
--- src/theme.js
+++
@@ -1,8 +0,0 @@
-export const theme = {
-   pallette: {
-      primary: '#1D1D1D',
-      textPrimary: '#E5E5E5',
-      textSecindary: '#848484',
-      background: '#121212',
-   },
-};
