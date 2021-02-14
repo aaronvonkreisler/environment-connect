5464b5bc259b5dc6b9afd89324689db1d478e491
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-02-14 12:38:52 -0600

    create notes doc upon plant creation

--- src/components/PlantDetails/Notes/index.js
+++ src/components/PlantDetails/Notes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NotesContainer, NotesBody } from './style';
 import TextEditor from 'components/PlantDetails/Notes/TextEditor';
+
 function Notes() {
    return (
       <NotesContainer>
