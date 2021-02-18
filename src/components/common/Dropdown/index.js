e92aa2e88cdedc3ac9254b475e014c827084087e
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-02-18 17:51:29 -0600

    dropdown component

---
+++ src/components/common/Dropdown/index.js
@@ -0,0 +1,52 @@
+import React, { useRef, useCallback, useEffect } from 'react';
+import { StyledUl, PillButton, StyledLi } from './style';
+import PropTypes from 'prop-types';
+
+export function Dropdown({ open, setOpen, labeledBy, id, children, ...props }) {
+   const ref = useRef();
+   const closeMenu = useCallback(
+      (e) => {
+         if (ref.current.contains(e.target)) {
+            return;
+         }
+         setOpen(false);
+         document.removeEventListener('mousedown', closeMenu);
+      },
+      [setOpen]
+   );
+
+   useEffect(() => {
+      if (open) {
+         document.addEventListener('mousedown', closeMenu);
+      }
+   }, [closeMenu, open]);
+   return (
+      <div style={{ position: 'relative' }}>
+         <PillButton
+            onClick={() => setOpen(!open)}
+            aria-expanded={open}
+            id={id}
+         >
+            <span> {labeledBy}</span>
+         </PillButton>
+         <StyledUl $open={open} ref={ref} aria-labelledby={id}>
+            {children}
+         </StyledUl>
+      </div>
+   );
+}
+
+Dropdown.propTypes = {
+   open: PropTypes.bool.isRequired,
+   setOpen: PropTypes.func.isRequired,
+   labeledBy: PropTypes.string.isRequired,
+   id: PropTypes.string.isRequired,
+   children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+   ]).isRequired,
+};
+
+export function DropdownItem({ ...props }) {
+   return <StyledLi {...props}>{props.children}</StyledLi>;
+}
---
+++ src/components/common/Dropdown/style.js
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components';
+
+export const StyledUl = styled.ul`
+   position: absolute;
+   top: 100%;
+   z-index: 1000;
+   display: none;
+   min-width: 8rem;
+   padding: 0.5rem 0;
+   font-size: 1rem;
+   list-style: none;
+   text-align: left;
+   background-color: #343a40;
+   background-clip: padding-box;
+   border: 1px solid rgba(0, 0, 0, 0.15);
+   border-radius: 0.25rem;
+   ${(props) =>
+      props.$open &&
+      css`
+         margin: 0px;
+         position: absolute;
+         inset: 0px auto auto 0px;
+         transform: translate(0px, 20px);
+         display: block;
+      `}
+`;
+
+export const PillButton = styled.button`
+   padding: 0.2em 0.55em;
+   font-size: 0.65em;
+   font-weight: 700;
+   background-color: #6c757d;
+   border-radius: 4px;
+   color: #212529;
+   min-width: 6rem;
+   cursor: pointer;
+
+   &:hover {
+      background-color: #535c64;
+      transition: background-color 0.2s ease-in;
+   }
+   // caret arrow
+   &::after {
+      display: inline-block;
+      margin-left: 0.255em;
+      vertical-align: 0.255em;
+      content: '';
+      border-top: 0.4em solid;
+      border-right: 0.4em solid transparent;
+      border-bottom: 0;
+      border-left: 0.4em solid transparent;
+   }
+`;
+
+export const StyledLi = styled.li`
+   padding: 0.2em 0.55em;
+   cursor: pointer;
+   &:hover {
+      background-color: hsla(210, 10%, 42%, 0.76);
+      transition: background-color 0.2s ease-in;
+   }
+`;
