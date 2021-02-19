9f28a149fbfe7eb82d74894b2021b9be083c46ca
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-02-19 13:01:57 -0600

    progressbar component

---
+++ src/components/common/Progressbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledBar, BarOne, BarTwo } from './style';
+
+function Progressbar() {
+   return (
+      <StyledBar>
+         <BarOne />
+         <BarTwo />
+      </StyledBar>
+   );
+}
+
+export default Progressbar;
