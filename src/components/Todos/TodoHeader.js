305e16e003603f56edd6db4417092c0b1242798c
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-03-06 20:18:41 -0600

    change text when filter is on todos

--- src/components/Todos/TodoHeader.js
+++ src/components/Todos/TodoHeader.js
@@ -6,65 +6,62 @@ import { Menu, MenuItem } from 'components/common/Menu';
 import IconButton from 'components/common/IconButton';
 
 function TodoHeader({
-   toggleForm,
-   setToggleForm,
-   requestSortBy,
-   showCompleted,
+  toggleForm,
+  setToggleForm,
+  requestSortBy,
+  showCompleted,
+  showingComplete,
 }) {
-   const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-   return (
-      <>
-         <TodoTitle>
-            <div>{toggleForm ? 'Add task' : 'My tasks'}</div>
-            <div>
-               {toggleForm && (
-                  <IconButton small grey onClick={() => setToggleForm(false)}>
-                     <MdArrowBack />
-                  </IconButton>
-               )}
-               {!toggleForm && (
-                  <>
-                     <IconButton
-                        small
-                        grey
-                        onClick={() => setToggleForm(!toggleForm)}
-                     >
-                        <BiPlus />
-                     </IconButton>
+  return (
+    <>
+      <TodoTitle>
+        <div>{toggleForm ? 'Add task' : 'My tasks'}</div>
+        <div>
+          {toggleForm && (
+            <IconButton small grey onClick={() => setToggleForm(false)}>
+              <MdArrowBack />
+            </IconButton>
+          )}
+          {!toggleForm && (
+            <>
+              <IconButton small grey onClick={() => setToggleForm(!toggleForm)}>
+                <BiPlus />
+              </IconButton>
 
-                     <IconButton
-                        small
-                        grey
-                        onClick={() => setMenuOpen(true)}
-                        aria-haspopup="true"
-                        aria-controls="task-options"
-                     >
-                        <MdMoreVert />
-                     </IconButton>
-                  </>
-               )}
+              <IconButton
+                small
+                grey
+                onClick={() => setMenuOpen(true)}
+                aria-haspopup="true"
+                aria-controls="task-options"
+              >
+                <MdMoreVert />
+              </IconButton>
+            </>
+          )}
 
-               <Menu
-                  open={menuOpen}
-                  id="task-options"
-                  onClose={() => setMenuOpen(false)}
-                  style={{ right: 0 }}
-               >
-                  <MenuItem onClick={() => requestSortBy('dueDate')}>
-                     Sort by due date
-                  </MenuItem>
-                  <MenuItem onClick={() => requestSortBy('priority')}>
-                     Sort by priority
-                  </MenuItem>
-                  <MenuItem onClick={() => showCompleted()}>
-                     Show completed tasks
-                  </MenuItem>
-               </Menu>
-            </div>
-         </TodoTitle>
-      </>
-   );
+          <Menu
+            open={menuOpen}
+            id="task-options"
+            onClose={() => setMenuOpen(false)}
+            style={{ right: 0 }}
+          >
+            <MenuItem onClick={() => requestSortBy('dueDate')}>
+              Sort by due date
+            </MenuItem>
+            <MenuItem onClick={() => requestSortBy('priority')}>
+              Sort by priority
+            </MenuItem>
+            <MenuItem onClick={() => showCompleted()}>
+              {showingComplete ? 'Show' : 'Hide'} completed tasks
+            </MenuItem>
+          </Menu>
+        </div>
+      </TodoTitle>
+    </>
+  );
 }
 
 export default TodoHeader;
--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -9,64 +9,61 @@ import useAuth from 'hooks/useAuth';
 import useSortableData from 'hooks/useSortableData';
 
 const initialState = {
-   todos: [],
-   incompleteTodos: [],
-   fetching: false,
-   error: null,
+  todos: [],
+  incompleteTodos: [],
+  fetching: false,
+  error: null,
 };
 function Todos() {
-   const { id } = useAuth();
-   const [toggleForm, setToggleForm] = useState(false);
-   const { state, toggleTodo, fetchTodos, addTodo } = useTodos(initialState);
-   const { todos, fetching, incompleteTodos } = state;
-   const [sortableData, setSortableData] = useState([]);
-   const [displayComplete, setDisplayComplete] = useState(false);
-   const { items, requestSortBy } = useSortableData(sortableData);
+  const { id } = useAuth();
+  const [toggleForm, setToggleForm] = useState(false);
+  const { state, toggleTodo, fetchTodos, addTodo } = useTodos(initialState);
+  const { todos, fetching, incompleteTodos } = state;
+  const [sortableData, setSortableData] = useState([]);
+  const [displayComplete, setDisplayComplete] = useState(false);
+  const { items, requestSortBy } = useSortableData(sortableData);
 
-   useEffect(() => {
-      fetchTodos(id);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, []);
+  useEffect(() => {
+    fetchTodos(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-   useEffect(() => {
-      if (incompleteTodos) {
-         setSortableData(incompleteTodos);
-      }
-   }, [incompleteTodos]);
+  useEffect(() => {
+    if (incompleteTodos) {
+      setSortableData(incompleteTodos);
+    }
+  }, [incompleteTodos]);
 
-   const showCompleted = () => {
-      if (displayComplete) {
-         setSortableData(incompleteTodos);
-         setDisplayComplete(false);
-      } else {
-         setSortableData(todos);
-         setDisplayComplete(true);
-      }
-   };
+  const showCompleted = () => {
+    if (displayComplete) {
+      setSortableData(incompleteTodos);
+      setDisplayComplete(false);
+    } else {
+      setSortableData(todos);
+      setDisplayComplete(true);
+    }
+  };
 
-   return (
-      <TodoCard>
-         <TodoHeader
-            toggleForm={toggleForm}
-            setToggleForm={setToggleForm}
-            requestSortBy={requestSortBy}
-            showCompleted={showCompleted}
-         />
-         {fetching && <TodoSkeleton />}
-         {toggleForm && (
-            <TodoForm
-               addTodo={addTodo}
-               setToggleForm={setToggleForm}
-               userId={id}
-            />
-         )}
-         {!toggleForm &&
-            !fetching &&
-            items.map((todo) => (
-               <TodoItem item={todo} toggleTodo={toggleTodo} key={todo.id} />
-            ))}
-      </TodoCard>
-   );
+  return (
+    <TodoCard>
+      <TodoHeader
+        toggleForm={toggleForm}
+        setToggleForm={setToggleForm}
+        requestSortBy={requestSortBy}
+        showCompleted={showCompleted}
+        showingComplete={!displayComplete}
+      />
+      {fetching && <TodoSkeleton />}
+      {toggleForm && (
+        <TodoForm addTodo={addTodo} setToggleForm={setToggleForm} userId={id} />
+      )}
+      {!toggleForm &&
+        !fetching &&
+        items.map((todo) => (
+          <TodoItem item={todo} toggleTodo={toggleTodo} key={todo.id} />
+        ))}
+    </TodoCard>
+  );
 }
 
 export default Todos;
